24fb1fb1d476424d80da534b99737d05a98e3d02
Graeme Richmond <richmond.graeme.com>
2024-10-14 12:47:50 +0100

    Added date picker, and custom styled components to AddBook component.

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -1,46 +1,67 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addBook } from "../features/bookReducer";
 import { createBook } from "../api/api";
+import { Heading2 } from "./Typography/Heading";
+import { TextInput } from "./Inputs/TextInput";
+import { ColContainer, RowContainer } from "./Layout/FlexContainer";
+import { BasicDatePicker } from "./Inputs/BasicDatePicker";
+import { Button1 } from "./Buttons/Button1";
+import FlexItem from "./Layout/FlexItem";
+import { colorMap } from "../styleVars/colors";
 
 const AddBook = () => {
-  const dispatch = useDispatch();
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [publishedDate, setPublishedDate] = useState("");
+    const dispatch = useDispatch();
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [publishedDate, setPublishedDate] = useState("");
 
-  const handleAddBook = async () => {
-    const newBook = await createBook({ title, author, publishedDate });
-    dispatch(addBook(newBook));
-    setTitle("");
-    setAuthor("");
-    setPublishedDate("");
-  };
+    const handleAddBook = async () => {
+        const newBook = await createBook({ title, author, publishedDate });
+        dispatch(addBook(newBook));
+        setTitle("");
+        setAuthor("");
+        setPublishedDate("");
+    };
 
-  return (
-    <div>
-      <h2>Add Book</h2>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Author"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Published Date"
-        value={publishedDate}
-        onChange={(e) => setPublishedDate(e.target.value)}
-      />
-      <button onClick={handleAddBook}>Add</button>
-    </div>
-  );
+    return (
+        <ColContainer>
+            <Heading2 color="white">Add Book</Heading2>
+            <RowContainer>
+                <TextInput
+                    type="text"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <TextInput
+                    type="text"
+                    placeholder="Author"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                />
+            </RowContainer>
+            <FlexItem
+                style={{
+                    backgroundColor: colorMap.orange300,
+                    padding: "10px",
+                    borderRadius: "4px",
+                }}
+            >
+                <BasicDatePicker
+                    label="Published Date"
+                    setDate={setPublishedDate}
+                />
+            </FlexItem>
+            <Button1
+                onClick={handleAddBook}
+                backgroundColorOnHover="blue500"
+                fontColorOnHover="white"
+            >
+                Add
+            </Button1>
+        </ColContainer>
+    );
 };
 
 export default AddBook;
