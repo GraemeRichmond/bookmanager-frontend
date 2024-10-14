d6173df66a3a57d44f3e23c7a4d3ee6e3d7cdcb6
Graeme Richmond <richmond.graeme.com>
2024-10-14 12:40:41 +0100

    Styled heading and layout of App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,42 @@ import { setBooks } from "./features/bookReducer";
 import BooksList from "./components/BooksList";
 import AddBook from "./components/AddBook";
 import { fetchBooks } from "./api/api";
+import { Heading1 } from "./components/Typography/Heading";
+import { colorMap } from "./styleVars/colors";
+import { RowContainer } from "./components/Layout/FlexContainer";
+import FlexItem from "./components/Layout/FlexItem";
 
 const App: React.FC = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    const loadBooks = async () => {
-      const books = await fetchBooks();
-      dispatch(setBooks(books));
-    };
+    useEffect(() => {
+        const loadBooks = async () => {
+            const books = await fetchBooks();
+            dispatch(setBooks(books));
+        };
 
-    loadBooks();
-  }, [dispatch]);
+        loadBooks();
+    }, [dispatch]);
 
-  return (
-    <div>
-      <h1>Book Management</h1>
-      <AddBook />
-      <BooksList />
-    </div>
-  );
+    return (
+        <div
+            style={{
+                padding: "20px",
+                backgroundColor: colorMap.charcoal400,
+                height: "100vh",
+            }}
+        >
+            <Heading1 color="orange400">Book Management</Heading1>
+            <RowContainer justifyContent="space-evenly" alignItems="start">
+                <FlexItem basis="50%">
+                    <AddBook />
+                </FlexItem>
+                <FlexItem basis="50%">
+                    <BooksList />
+                </FlexItem>
+            </RowContainer>
+        </div>
+    );
 };
 
 export default App;
