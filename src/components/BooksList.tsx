c822f305de96c19d7b785a20025b44e1fdecb59b
Graeme Richmond <richmond.graeme.com>
2024-10-14 12:50:20 +0100

    Added BookCard to book list component. Changed layout and heading.

--- src/components/BooksList.tsx
+++ src/components/BooksList.tsx
@@ -1,22 +1,36 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+import { Heading2 } from "./Typography/Heading";
+import { ColContainer } from "./Layout/FlexContainer";
+import BookCard from "./BookCard";
 
 const BooksList = () => {
-  const books = useSelector((state: RootState) => state.books.books);
+    const books = useSelector((state: RootState) => state.books.books);
 
-  return (
-    <div>
-      <h2>Books</h2>
-      <ul>
-        {books.map((book) => (
-          <li key={book.id}>
-            {book.title} by {book.author} (Published: {book.publishedDate})
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <ColContainer>
+            <Heading2 color="white">Books</Heading2>
+            <ul style={{ padding: 0 }}>
+                {books.map((book) => (
+                    <li
+                        key={book.id}
+                        style={{
+                            marginBottom: "16px",
+                            listStyleType: "none",
+                            maxWidth: "40vw", // <--- Temporary fix to prevent overflow
+                        }}
+                    >
+                        <BookCard
+                            title={book.title}
+                            author={book.author}
+                            publishedDate={book.publishedDate}
+                            id={book.id}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </ColContainer>
+    );
 };
 
 export default BooksList;
